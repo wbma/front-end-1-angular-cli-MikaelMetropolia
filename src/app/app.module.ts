2d0735a9e1ceb959ad8a571d0d07f2483b9f30ae
Mikael Ramo <Mikaelhr@metropolia.fi>
2018-01-16 12:16:48 +0200

    Task 1b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    MainComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
